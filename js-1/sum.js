1dcc83d5a9ec94f2b71a367af8c112212aeaa94c
Rodri <126527883+r0odr1@users.noreply.github.com>
2023-05-10 19:12:59 -0500

    Add files via upload

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,8 +1,16 @@
-function suma(numero){
+/*function suma(numero){
     let total = numero
     for(let i = 1; i < numero; i++){
         total = total + i
     }return total
+}
+console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
+console.log(suma(10)) // 55
+console.log(suma(15)) // 120*/
+
+
+function suma(numero){
+    return (numero * (numero + 1)) / 2    
 }
 console.log(suma(4)) // 1 + 2 + 3 + 4 = 10
 console.log(suma(10)) // 55
